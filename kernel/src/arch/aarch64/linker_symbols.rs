98839a5bf36b9d4b2dfe0427861396135bdefb0f
Ulrich <ulrich_tan@yahoo.fr>
2026-06-03 23:43:03 +0000

    fix: linker_symbols.rs

--- kernel/src/arch/aarch64/linker_symbols.rs
+++ kernel/src/arch/aarch64/linker_symbols.rs
@@ -1,12 +1,22 @@
 extern "C" {
+    // Kernel sections
     pub static _text_start: u8;
     pub static _text_end: u8;
+
     pub static _rodata_start: u8;
     pub static _rodata_end: u8;
+
     pub static _data_start: u8;
     pub static _data_end: u8;
+
     pub static _bss_start: u8;
     pub static _bss_end: u8;
+
+    // Stack
+    pub static _stack_start: u8;
+    pub static _stack_top: u8;
+
+    // Kernel global bounds
     pub static _kernel_start: u8;
     pub static _kernel_end: u8;
 }
